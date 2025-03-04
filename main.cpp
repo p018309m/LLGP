9987104ff97f12944eded40eba6d2f60c2abde72
Rajat Rajendra Prabhu <114941374+p018309m@users.noreply.github.com>
2025-03-04 17:58:35 +0000

    Fun Stuff added
    Added in a shape which rotates at its center and then moves around within the bounds of the window

--- main.cpp
+++ main.cpp
@@ -5,9 +5,28 @@
 */
 int WinMain()
 {
-    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "SFML works!");
+    sf::ConvexShape convex;
+
+    //Set Convex Shape Points Here
+    convex.setFillColor(sf::Color::White);
+    convex.setPointCount(6);
+    convex.setPoint(0, { 75.f, 25.f });
+    convex.setPoint(1, { 50.f,50.f });
+    convex.setPoint(2, { 50.f,100.f });
+    convex.setPoint(3, { 75.f,125.f });
+    convex.setPoint(4, { 100.f,100.f });
+    convex.setPoint(5, { 100.f,50.f });
+    convex.setScale({ 2.f,2.f });
+
+    //Set Convex Shape Transform here
+    sf::Vector2f shapePosition(500.f, 500.f);
+    convex.setPosition(shapePosition);
+    convex.setOrigin({ 75.f,75.f });
+
+    //Movement Variables
+    float xVelocity = 2.f;
+    float yVelocity = 5.f;
 
     while (window.isOpen())
     {
@@ -17,10 +36,20 @@ int WinMain()
         {
             if (event->is<sf::Event::Closed>())
                 window.close();
+
+            //Movement within bounds of the window
+            shapePosition.x += xVelocity;
+            shapePosition.y += yVelocity;
+            convex.setPosition(shapePosition);
+            convex.rotate(sf::degrees(10.f));
+            if (shapePosition.x > 1280 - 100 || shapePosition.x < 0)
+                xVelocity *= -1;
+            if (shapePosition.y > 720 - 100 || shapePosition.y < 0)
+                yVelocity *= -1;
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(convex);
         window.display();
     }
 }
