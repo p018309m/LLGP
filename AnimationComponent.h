0ce3810e6c37e80cefca07bf27eaeea0d51d5fa7
Rajat Rajendra Prabhu <114941374+p018309m@users.noreply.github.com>
2025-04-04 16:53:35 +0100

    Added New Spritesheet
    Animation Component Added and tried doing SAT Collision

---
+++ AnimationComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+class AnimationComponent
+{
+private:
+
+public:
+	//Constructors & Destructors
+	AnimationComponent();
+	virtual ~AnimationComponent();
+};
+
---
+++ SATCollision.cpp
@@ -0,0 +1,1 @@
+#include "SATCollision.h"
